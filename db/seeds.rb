64d2e40393d2e1349eb5298f489fa706e97a399f
Pranav Misal <pranav.misal@definelabs.in>
2020-05-26 12:45:32 +0530

    Build user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Evgeny", password: "123456")
+User.create(username: "Jonsnow", password: "123456")
+User.create(username: "Arya", password: "123456")
+User.create(username: "Frodo", password: "123456")
+User.create(username: "Gandalf", password: "123456")
